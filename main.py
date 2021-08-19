b8ec0902cd5ded30db706be4f59bb0aacf6387b9
Luanda Dantas Leite <39995207+luandadantas@users.noreply.github.com>
2021-08-19 10:28:41 -0300

    Pega Diário Oficial e posta no twitter (#10)
    * Pega diario oficial e posta no twitter
    
    * code review fixes
    
    * ajustes solicitados
    
    * removi linha em branco do .env-sample

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
+from requests.models import Response
 import tweepy
-
+import requests
+import json
+import datetime
 from loguru import logger
 from dotenv import load_dotenv
-
 
 load_dotenv()
 
@@ -14,3 +16,36 @@ def tweet(message: str):
 
     api = tweepy.API(auth)
     api.update_status(message)
+
+
+def get_todays_gazette():
+    gazettes = []
+
+    date_today = datetime.date.today().strftime('%Y-%m-%d')
+
+    params = {'start_date': date_today}
+    url = os.getenv('MARIA_QUITERIA_API_URL')
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': os.getenv('MARIA_QUITERIA_API_TOKEN')
+    }
+    
+    response = requests.get(url, headers=headers, params=params)
+    response_json = response.json()
+
+    for result in response_json['results']:
+        gazettes.append(result)
+
+    return gazettes
+
+
+def post_todays_gazette(gazettes: list):
+    date_today = datetime.date.today().strftime('%Y-%m-%d')
+    for gazette in gazettes:
+        tweet_message = f"Saiu uma nova edição do #DiárioOficial do poder {gazette['power']} de #FeiradeSantana ({date_today} - {gazette['year_and_edition']}).\n{gazette['files'][0]['url']}"
+        tweet(tweet_message)
+
+
+if __name__ == '__main__':
+    gazettes = get_todays_gazette()
+    post_todays_gazette(gazettes)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,51 @@
 from unittest import mock
-from main import tweet
+import pytest
+import requests
+import json
+from main import tweet, get_todays_gazette
 from unittest.mock import call
 
 
 def test_if_tweet_was_post_on_twitter(mocker):
     mock_tweepy = mocker.patch('main.tweepy')
-
     tweet('Hi')
 
     assert mock_tweepy.OAuthHandler.called
     assert mock_tweepy.API.called
     assert call().update_status('Hi') in mock_tweepy.API.mock_calls
+
+
+def test_if_get_todays_gazette_return_result(mocker):
+    expected_result = {
+        "count": 249,
+        "next": "...",
+        "previous": None,
+        "results": [{
+            "crawled_from": "...",
+            "date": "2021-08-14",
+            "power": "legislativo",
+            "year_and_edition": "Ano xxx - Edição Nº xxx",
+            "events": [
+                {
+                    "title": "TEXT",
+                    "secretariat": "TEXT",
+                    "summary": "TEXT",
+                    "published_on": None
+                }
+            ],
+            "files": [
+                {
+                    "url": "..."
+                }
+            ]
+        }]
+    }
+
+    mock_response = mocker.patch('main.requests.get')
+    mock_response.return_value.ok = True
+    mock_response.return_value.json.return_value = expected_result
+    
+    result = get_todays_gazette()
+
+    assert mock_response.called
+    assert result != []
