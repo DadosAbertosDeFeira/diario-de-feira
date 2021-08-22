2ca6c79dbb930c69800c193e16c9740a056431d1
Ana Paula Gomes <apgomes88@gmail.com>
2021-08-22 20:27:29 +0200

    Formata código

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-import os
-from requests.models import Response
-import tweepy
-import requests
-import json
 import datetime
-from loguru import logger
+import os
+
+import requests
+import tweepy
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
 def tweet(message: str):
-    auth = tweepy.OAuthHandler(os.getenv('CONSUMER_KEY'), os.getenv('CONSUMER_SECRET'))
-    auth.set_access_token(os.getenv('ACCESS_TOKEN'), os.getenv('ACCESS_TOKEN_SECRET'))
+    auth = tweepy.OAuthHandler(os.getenv("CONSUMER_KEY"), os.getenv("CONSUMER_SECRET"))
+    auth.set_access_token(os.getenv("ACCESS_TOKEN"), os.getenv("ACCESS_TOKEN_SECRET"))
 
     api = tweepy.API(auth)
     api.update_status(message)
@@ -21,31 +19,35 @@ def tweet(message: str):
 def get_todays_gazette():
     gazettes = []
 
-    date_today = datetime.date.today().strftime('%Y-%m-%d')
+    date_today = datetime.date.today().strftime("%Y-%m-%d")
 
-    params = {'start_date': date_today}
-    url = os.getenv('MARIA_QUITERIA_API_URL')
+    params = {"start_date": date_today}
+    url = os.getenv("MARIA_QUITERIA_API_URL")
     headers = {
-        'Content-Type': 'application/json',
-        'Authorization': os.getenv('MARIA_QUITERIA_API_TOKEN')
+        "Content-Type": "application/json",
+        "Authorization": os.getenv("MARIA_QUITERIA_API_TOKEN"),
     }
-    
+
     response = requests.get(url, headers=headers, params=params)
     response_json = response.json()
 
-    for result in response_json['results']:
+    for result in response_json["results"]:
         gazettes.append(result)
 
     return gazettes
 
 
 def post_todays_gazette(gazettes: list):
-    date_today = datetime.date.today().strftime('%Y-%m-%d')
+    date_today = datetime.date.today().strftime("%Y-%m-%d")
     for gazette in gazettes:
-        tweet_message = f"Saiu uma nova edição do #DiárioOficial do poder {gazette['power']} de #FeiradeSantana ({date_today} - {gazette['year_and_edition']}).\n{gazette['files'][0]['url']}"
+        tweet_message = (
+            f"Saiu uma nova edição do #DiárioOficial do poder {gazette['power']} "
+            f"de #FeiradeSantana ({date_today} - {gazette['year_and_edition']}). 📰\n"
+            f"{gazette['files'][0]['url']}"
+        )
         tweet(tweet_message)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     gazettes = get_todays_gazette()
     post_todays_gazette(gazettes)
