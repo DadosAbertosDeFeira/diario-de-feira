19ed1935b157b94a75672b42965c54343ed2b3bc
Ana Paula Gomes <apgomes88@gmail.com>
2021-09-25 13:38:58 +0200

    Adiciona pacote python

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="diario",
+    version="1.0",
+    description="O que acontece no Diário Oficial no seu Twitter",
+    author="Dados Abertos de Feira",
+    author_email="dadosabertosdefeira@gmail.com",
+    url="https://www.dadosabertosdefeira.com.br",
+    packages=["diario"],
+)
